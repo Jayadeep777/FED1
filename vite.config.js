fb3afa1c72d6c72bef6068d29adb655338b5786c
Jayadeep777 <jayadeepbavirisetty@gmail.com>
2025-07-25 09:39:57 +0530

    Setup for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/hello-world-app/',  // Replace with your actual repo name
+  base: '/FED/',  // Replace with your actual repo name
   plugins: [react()],
 })
